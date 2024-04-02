08b881de84e9698253405bb89ee944f714127c99
anisa <you@example.com>
2024-04-02 00:34:44 -0700

    Created two testng test methods

---
+++ src/test/java/com/allianz/test/LoginUITest.java
@@ -0,0 +1,21 @@
+package com.allianz.test;
+
+import org.testng.annotations.Test;
+
+public class LoginUITest {
+	@Test(priority = 1)
+	public void titleTest()
+	{
+		System.out.println("titleTest");
+	}
+	@Test(priority = 2)
+	public void applicationDescriptionTest()
+	{
+		System.out.println("AppTest");
+	}
+	@Test(invocationCount = 2)
+	public void CountTest()
+	{
+		System.out.println("countTest");
+	}
+}
